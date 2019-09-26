c2c73315db7c39b0137f6e597856c7e109433ce9
Karly Kizewski <kizewski.karly@gmail.com>
2019-09-26 18:35:41 +0000

    new action and page with simple form installed and inserted onto new task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,12 @@
 class TasksController < ApplicationController
   def index
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+
+  end
 end
--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -8,4 +8,20 @@ RSpec.describe TasksController, type: :controller do
     end
   end
 
+  describe "tasks#new action" do
+    it "should successfully show the new form" do
+      get :new
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "tasks#create action" do
+    it "should successfully create a new task in the database" do
+      post :create, params: { task: { message: 'Get home together!' } }
+      expect(response).to redirect_to root_path
+
+      task = Task.last
+      expect(task.message).to eq("Get home together!")
+    end
+  end
 end
