d538f3983730d063df74a78bb816791527916160
Karly Kizewski <kizewski.karly@gmail.com>
2019-09-29 19:55:38 +0000

    create spec test passes

--- app/controllers/cleans_controller.rb
+++ app/controllers/cleans_controller.rb
@@ -1,10 +1,25 @@
 class CleansController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:new, :create]
   def index
     @cleans = Clean.all
   end
 
   def new
     @clean = Clean.new
+  end
+
+  def create
+    @clean = current_user.cleans.create(clean_params)
+    if @clean.valid?
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def clean_params
+    params.require(:clean).permit(:message)
   end
 end
