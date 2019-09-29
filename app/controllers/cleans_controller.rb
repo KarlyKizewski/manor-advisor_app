ee66d6f9021b0690bb862ffeed07b89a13186cd0
Karly Kizewski <kizewski.karly@gmail.com>
2019-09-29 19:46:40 +0000

    spec test for new clean passes

--- app/controllers/cleans_controller.rb
+++ app/controllers/cleans_controller.rb
@@ -1,6 +1,10 @@
 class CleansController < ApplicationController
-
+  before_action :authenticate_user!, only: [:new]
   def index
     @cleans = Clean.all
+  end
+
+  def new
+    @clean = Clean.new
   end
 end
--- spec/controllers/cleans_controller_spec.rb
+++ spec/controllers/cleans_controller_spec.rb
@@ -7,5 +7,18 @@ RSpec.describe CleansController, type: :controller do
     end
   end
 
+  describe "cleans#new action" do
+    it "should require users to be logged in" do
+      get :new
+      expect(response).to redirect_to new_user_session_path
+    end
+
+    it "should successfully show the new form" do
+      user = FactoryBot.create(:user)
+      sign_in user
+      get :new
+      expect(response).to have_http_status(:success)
+    end
+  end
 
 end
