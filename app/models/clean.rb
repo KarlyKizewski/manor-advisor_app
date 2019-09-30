f24269ac6632b8da2ed6d4e3b080e43256792051
Karly Kizewski <kizewski.karly@gmail.com>
2019-09-30 16:59:30 +0000

    css changes

--- app/models/clean.rb
+++ app/models/clean.rb
@@ -1,6 +1,5 @@
 class Clean < ApplicationRecord
-  validates :date, presence: true
+  validates :message, presence: true
 
   belongs_to :user
-  has_many :dates
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "tasks#index"
+  root "static_pages#index"
   resources :tasks
   resources :cleans
   get 'interval_tasks', to: "tasks#interval_tasks"
